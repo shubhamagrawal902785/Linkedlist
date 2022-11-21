8c452fb91df56b2e4483d559e8905f9e80d91f0c
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-21 23:19:43 +0530

    Added: Element search

--- src/com/bridgelabz/com/CreateLinklist.java
+++ src/com/bridgelabz/com/CreateLinklist.java
@@ -2,146 +2,168 @@ package com.bridgelabz.com;
 import org.w3c.dom.Node;
 public class CreateLinklist {
 	
-	    Node head;
-	    class Node {
-	        String data; 
-	        Node next;   
+	Node head;
+    class Node {
+        String data; 
+        Node next;   
 
-	        Node(String data){
-	            this.data = data;
-	            this.next = null;
-	        }
-	    }
-	    //Adding the data in first position
-	    public void addFirst(String data){
-	        Node newNode = new Node(data); 
-	        if(head == null){              
-	            head = newNode;
-	            return;
-	        }
-	        newNode.next = head;
-	        head = newNode;
-	    }
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    //Adding the data in first position
+    public void addFirst(String data){
+        Node newNode = new Node(data); 
+        if(head == null){              
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
 
-	    // Adding the data to the last
-	    public void addLast(String data){
-	        Node newNode = new Node(data);
-	        if(head == null){
-	            head = newNode;
-	            return;
-	        }
-	        Node tempNode = head;
-	        while(tempNode.next != null){
-	            tempNode= tempNode.next;
-	        }
-	        tempNode.next = newNode;
-	    }
-	    //Printing the code
-	    void Print() {
-	        if (head == null) {                                  
-	            System.out.println("Linked list is empty...");
-	            return;
-	        }
-	        Node tempNode = head;                                 
-	        while (tempNode != null) {                          
-	            System.out.print(tempNode.data + "-");         
-	            tempNode = tempNode.next;                       
-	        }
-	    }
+    // Adding the data to the last
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node tempNode = head;
+        while(tempNode.next != null){
+            tempNode= tempNode.next;
+        }
+        tempNode.next = newNode;
+    }
+    //Printing the code
+    void Print() {
+        if (head == null) {                                  // Checking f head is empty or not
+            System.out.println("Linked list is empty...");
+            return;
+        }
+        Node tempNode = head;                                 // Taking temp node
+        while (tempNode != null) {                          // Running the temp node throughout the linked list
+            System.out.print(tempNode.data + "-");         // Printing temp node data
+            tempNode = tempNode.next;                       // Storing next node in temp data
+        }
+    }
 
-	    // Counting the size
-	    public int size() {
-	        int count = 0;
+    // Counting the size
+    public int size() {
+        int count = 0;
 
-	        Node temp = head;
+        Node temp = head;
 
-	        while (temp != null) {
-	            count++;
-	            temp = temp.next;
-	        }
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
 
-	        return count;
+        return count;
 
-	    }
-	        // inserting the data in between
-	    public void insertIndex(int userIndex, String data) {
-	    	CreateLinklist obj1 = new CreateLinklist();
+    }
+        // inserting the data in between
+    public void insertIndex(int userIndex, String data) {
+    	CreateLinklist obj1 = new CreateLinklist();
 
-	        // User trying to insert a first position
-	        if (userIndex == 0)
-	            obj1.addFirst(data);
+        // User trying to insert a first position
+        if (userIndex == 0)
+            obj1.addFirst(data);
 
-	            // User trying to insert a last position
-	        else if (userIndex == size())
-	            obj1.addLast(data);
+            // User trying to insert a last position
+        else if (userIndex == size())
+            obj1.addLast(data);
 
-	            // User trying to insert a invalid position
-	        else if (userIndex < 0 || userIndex >= size())
-	            System.out.println("Invalid index");
+            // User trying to insert a invalid position
+        else if (userIndex < 0 || userIndex >= size())
+            System.out.println("Invalid index");
 
-	            // User trying to insert a specific index
-	        else {
-	            // Creating New Node
-	            Node newNode = new Node(data);
+            // User trying to insert a specific index
+        else {
+            // Creating New Node
+            Node newNode = new Node(data);
 
-	            // To track traversing
-	            int index = 0;
+            // To track traversing
+            int index = 0;
 
-	            // Pointers to track left & right side elements
-	            Node left = head;
-	            Node right = left.next;
+            // Pointers to track left & right side elements
+            Node left = head;
+            Node right = left.next;
 
-	            // Traverse or Move till last element before user entered index
-	            while (index < userIndex - 1) {
-	                left = left.next;
-	                right = right.next;
-	                index++;
-	            }
-	            // Connecting new Node with right side elements
-	            newNode.next = right;
+            // Traverse or Move till last element before user entered index
+            while (index < userIndex - 1) {
+                left = left.next;
+                right = right.next;
+                index++;
+            }
+            // Connecting new Node with right side elements
+            newNode.next = right;
 
-	            // Connecting left side elements with new Node
-	            left.next = newNode;
-	        }
-	    }
+            // Connecting left side elements with new Node
+            left.next = newNode;
+        }
+    }
 
-	    // Deleting the first element
-	    public void deleteFirst(){
-	        if(head == null){
-	            System.out.println(" the list is empty");
-	            return;
-	        }
-	        head = head.next;
-	    }
+    // Deleting the first element
+    public void deleteFirst(){
+        if(head == null){
+            System.out.println(" the list is empty");
+            return;
+        }
+        head = head.next;
+    }
 
-	    // deleting the last element
-	    public void deleteLast(){
-	        if(head == null){
-	            System.out.println(" the list is empty");
-	            return;
-	        }
-	        if(head.next == null){
-	            head= null;
-	            return;
-	        }
-	        Node secoundLast = head;
-	        Node lastNode = head.next;
-	        while(lastNode.next != null){
-	            lastNode = lastNode.next;
-	            secoundLast = secoundLast.next;
-	        }
-	        secoundLast.next= null;
-	    }
+    // deleting the last element
+    public void deleteLast(){
+        if(head == null){
+            System.out.println(" the list is empty");
+            return;
+        }
+        if(head.next == null){
+            head= null;
+            return;
+        }
+        Node secoundLast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secoundLast = secoundLast.next;
+        }
+        secoundLast.next= null;
+    }
 
-	    public static void main(String[] args) {
-	    	CreateLinklist obj = new CreateLinklist();
+    // Ability to search
+    public void Search(String Data1) {
+        if (head == null) {                                  // Checking f head is empty or not
+            System.out.println("Linked list is empty...");
+            return;
+        }
+        int check = 0;
+        Node tempNode = head;                                 // Taking temp node
+        while (tempNode != null) {
+            check++;
+            if (tempNode.data == Data1) {
+                System.out.println(" element is found" + tempNode.data + " at " + check);
+                break;
+            }else {
+                tempNode = tempNode.next;
+            }
+        }
+        if (tempNode == null){
+            System.out.println(" Elemnet not found");
+        }
+    }
+    public static void main(String[] args) {
+    	CreateLinklist obj = new CreateLinklist();
 
-	        obj.addFirst("56");
-	        obj.addFirst("30");
-	        obj.addFirst("70");
-	        obj.insertIndex(2, "");
-	       obj.deleteFirst();
-	        //obj.deleteLast();
-	        obj.Print();
-	    }
-	}
+        obj.addFirst("56");
+        obj.addFirst("30");
+        obj.addFirst("70");
+        obj.insertIndex(2, "");
+        //obj.deleteFirst();
+        //obj.deleteLast();
+        obj.Search("30");
+        obj.Print();
+    }
+}
