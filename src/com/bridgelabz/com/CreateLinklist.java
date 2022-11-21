713fc86a5d194fdd335bf13562302ac7c636367a
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-21 15:11:34 +0530

    Added: insert element in linked list

--- src/com/bridgelabz/com/CreateLinklist.java
+++ src/com/bridgelabz/com/CreateLinklist.java
@@ -10,7 +10,7 @@ public class CreateLinklist {
 	            this.next = null;
 	        }
 	    }
-	    //Adding the data in first position
+	    //Adding the data in first 
 	    public void addFirst(String data){
 	        Node newNode = new Node(data); 
 	        if(head == null){              
@@ -21,7 +21,7 @@ public class CreateLinklist {
 	        head = newNode;
 	    }
 
-	    // Adding the data to the last
+	    // Adding the data last 
 	    public void addLast(String data){
 	        Node newNode = new Node(data);
 	        if(head == null){
@@ -34,26 +34,82 @@ public class CreateLinklist {
 	        }
 	        tempNode.next = newNode;
 	    }
-	    //Printing the code
+	   
 	    void Print() {
-	        if (head == null) {                                  
+	        if (head == null) {                                  // Checking f head is empty or not
 	            System.out.println("Linked list is empty...");
 	            return;
 	        }
-	        Node tempNode = head;                                 
-	        while (tempNode != null) {                          
-	            System.out.print(tempNode.data + "-");         
-	            tempNode = tempNode.next;                      
+	        Node tempNode = head;                                 // Taking temp node
+	        while (tempNode != null) {                          // Running the temp node throughout the linked list
+	            System.out.print(tempNode.data + "-");         // Printing temp node data
+	            tempNode = tempNode.next;                       // Storing next node in temp data
 	        }
 	    }
-	    
+
+	    // 
+	    public int size() {
+	        int count = 0;
+
+	        Node temp = head;
+
+	        while (temp != null) {
+	            count++;
+	            temp = temp.next;
+	        }
+
+	        return count;
+
+	    }
+	        // inserting the data in between
+	    public void insertIndex(int userIndex, String data) {
+	    	CreateLinklist obj1 = new CreateLinklist();
+
+	        // User trying to insert a first position
+	        if (userIndex == 0)
+	            obj1.addFirst(data);
+
+	            // User trying to insert a last position
+	        else if (userIndex == size())
+	            obj1.addLast(data);
+
+	            // User trying to insert a invalid position
+	        else if (userIndex < 0 || userIndex >= size())
+	            System.out.println("Invalid index");
+
+	            // User trying to insert a specific index
+	        else {
+	            // Creating New Node
+	            Node newNode = new Node(data);
+
+	            // To track traversing
+	            int index = 0;
+
+	            // Pointers to track left & right side elements
+	            Node left = head;
+	            Node right = left.next;
+
+	            // Traverse or Move till last element before user entered index
+	            while (index < userIndex - 1) {
+	                left = left.next;
+	                right = right.next;
+	                index++;
+	            }
+	            // Connecting new Node with right side elements
+	            newNode.next = right;
+
+	            // Connecting left side elements with new Node
+	            left.next = newNode;
+	        }
+	    }
+
 	    public static void main(String[] args) {
 	    	CreateLinklist obj = new CreateLinklist();
 
 	        obj.addFirst("56");
 	        obj.addFirst("30");
 	        obj.addFirst("70");
-
+	        obj.insertIndex(2, "" );
 	        obj.Print();
 	    }
-}
+	}
