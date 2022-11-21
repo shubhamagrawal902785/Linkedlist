cbdfccfd0b162bc298157e0b6c6e54ec72721876
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-21 14:57:23 +0530

    Added: Append link list

--- src/com/bridgelabz/com/CreateLinklist.java
+++ src/com/bridgelabz/com/CreateLinklist.java
@@ -1,45 +1,59 @@
 package com.bridgelabz.com;
-
+import org.w3c.dom.Node;
 public class CreateLinklist {
-	Node head;
-	class Node {
-	String data;
-	Node next;
+	 Node head;
+	    class Node {
+	        String data; 
+	        Node next;   
+	        Node(String data){
+	            this.data = data;
+	            this.next = null;
+	        }
+	    }
+	    //Adding the data in first position
+	    public void addFirst(String data){
+	        Node newNode = new Node(data); 
+	        if(head == null){              
+	            head = newNode;
+	            return;
+	        }
+	        newNode.next = head;
+	        head = newNode;
+	    }
 
-	Node(String data){
-	this.data = data;
-	this.next = null;
-	}
-	}
+	    // Adding the data to the last
+	    public void addLast(String data){
+	        Node newNode = new Node(data);
+	        if(head == null){
+	            head = newNode;
+	            return;
+	        }
+	        Node tempNode = head;
+	        while(tempNode.next != null){
+	            tempNode= tempNode.next;
+	        }
+	        tempNode.next = newNode;
+	    }
+	    //Printing the code
+	    void Print() {
+	        if (head == null) {                                  
+	            System.out.println("Linked list is empty...");
+	            return;
+	        }
+	        Node tempNode = head;                                 
+	        while (tempNode != null) {                          
+	            System.out.print(tempNode.data + "-");         
+	            tempNode = tempNode.next;                      
+	        }
+	    }
+	    
+	    public static void main(String[] args) {
+	    	CreateLinklist obj = new CreateLinklist();
 
-	public void addFirst(String data){
-	Node newNode = new Node(data);
-	if(head == null){
-	head = newNode;
-	return;
-	}
-	newNode.next = head;
-	head = newNode;
-	}
-	public void printNode(){
-	if(head==null){
-	System.out.println("Linked list is empty...");
-	return;
-	}
-	Node tempNode = head;
-	while(tempNode != null){
-	System.out.print(tempNode.data + "=>");
-	tempNode = tempNode.next;
-	}
-	System.out.println("null");
-	}
+	        obj.addFirst("56");
+	        obj.addFirst("30");
+	        obj.addFirst("70");
 
-	public static void main(String[] args) {
-		CreateLinklist ll = new CreateLinklist();
-	ll.addFirst("70");
-	ll.addFirst("30");
-	ll.addFirst("56");
-	ll.printNode();
-	}
-	
+	        obj.Print();
+	    }
 }
